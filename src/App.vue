a4a699c5749ea39bda8996042947225c4b98a5f5
Mayank Tomar <40265635+0Mayank@users.noreply.github.com>
2022-05-14 13:27:06 +0530

    add vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,26 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import Sidebar from "./components/Sidebar.vue";
-import Cards from "./components/Cards.vue";
-import Searchbar from "./components/Searchbar.vue";
-</script>
-
 <template>
-	<div className="main-container">
-		<div className="sidebar-container">
-			<Sidebar />
-		</div>
-		<div className="cards-container">
-			<Searchbar />
-			<Cards />
-		</div>
-	</div>
+  <v-app>
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
 </template>
 
-<style scoped>
-.main-container {
-	display: flex;
-	background-color: #113536;
-	gap: 23px;
-	/* min-height: auto; */
-}
-.sidebar-container {
-	flex: 1;
-	background-color: #112123;
-}
-.cards-container {
-	flex: 3.5;
-}
-</style>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
 
-<style>
-body,
-html,
-#app {
-	/* height: 100%; */
-	/* min-height: 1000px; */
-	margin: 0;
-	background-color: red;
-}
-</style>
+export default defineComponent({
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data () {
+    return {
+      //
+    }
+  },
+})
+</script>
