055bee8178b29b598e3cc0982a8743080fb5e8a0
Mayank Tomar <40265635+0Mayank@users.noreply.github.com>
2022-05-26 20:30:07 +0530

    added overlay to card and slide-fade transition

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,25 +1,34 @@
 <template>
-    <v-card width="310" class="rounded-card pa-1" color="primary">
-        <v-img class="rounded-card bg-image" :src="bg"></v-img>
-        
-        <v-sheet class="game-icon rounded-card" width="60" color="primary" elevation="7">
-            <v-img class="rounded-card" width="60" :src="icon"></v-img>
-        </v-sheet>
+    <v-hover v-slot="{ isHovering, props }">    
+        <v-card width="310" class="rounded-card pa-1 ma-3" color="primary" v-bind="props">
+            
+            <v-img class="rounded-card" cover aspect-ratio="0.6666667" :src="bg"></v-img>
+            
+            <div v-if="isHovering" class="overlay rounded-card"></div>
 
-        <v-card-title>{{ title }}</v-card-title>
-        
-        <v-card-actions v-if="last_played">
-            <v-btn class="act-btn" rounded>
-                <v-icon>mdi-circle-medium</v-icon>
-                Last Played: {{ last_played }}
-            </v-btn>
-            <v-spacer/>
-            <v-btn class="act-btn" rounded>
-                <v-icon>mdi-circle-medium</v-icon>
-                Last Saved: {{ last_saved }}
-            </v-btn>
-        </v-card-actions>
-    </v-card>
+            <transition name="slide-fade">
+                <div v-if="isHovering" class="hover-div rounded-card">   
+                    <!-- <v-sheet class="game-icon rounded-card" width="60" color="primary" elevation="7">
+                        <v-img class="rounded-card" width="60" :src="icon"></v-img>
+                    </v-sheet> -->
+
+                    <v-card-title class="text-h5 font-weight-bold">{{ title }}</v-card-title>
+                    
+                    <v-card-actions v-if="last_played">
+                        <v-btn class="act-btn" rounded>
+                            <v-icon>mdi-circle-medium</v-icon>
+                            Last Played: {{ last_played }}
+                        </v-btn>
+                        <v-spacer/>
+                        <v-btn class="act-btn" rounded>
+                            <v-icon>mdi-circle-medium</v-icon>
+                            Last Saved: {{ last_saved }}
+                        </v-btn>
+                    </v-card-actions>
+                </div>
+            </transition>
+        </v-card>
+    </v-hover>
 </template>
 
 <script lang="ts">
@@ -50,19 +59,28 @@ export default {
 </script>
 
 <style scoped>
-.bg-image {
-    filter: blur(1px);
-}
 .rounded-card {
     border-radius: 20px;
 }
 
 .game-icon {
     margin-left: 20px;
-    margin-top: -30px;
 }
 
 .act-btn {
     font-size: 10px;
+}
+
+.overlay{
+    background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.7));
+    width: 100%;
+    height: 50%;
+    margin-top: -226.5px;
+    transform: translate(0, 0);
+}
+
+.hover-div {
+    margin-top: -100px;
+    transform: translate(0, 0);
 }
 </style>
--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -4,14 +4,46 @@
 		<Searchbar />
 		
 		<v-row align-items="right">
-				<!-- v-col might have a bug so not using rn -->
-				<Card class="ma-3" v-for="_ in 7" :key="card-_"
-					bg="https://media.discordapp.net/attachments/747692657131651175/973511976665899028/20220510_043136.jpg" 
-					icon="https://cdn.discordapp.com/emojis/921067306363289640.webp?size=128&quality=lossless"
-					title="Blon"
-					last_played="3/6/23"
-					last_saved="3/6/23"
-					/>
+			<!-- v-col might have a bug so not using rn -->
+			<Card class="ma-3"
+				bg="https://thumbnails.pcgamingwiki.com/7/7c/Elden_Ring_-_cover.jpg/300px-Elden_Ring_-_cover.jpg" 
+				icon="https://styles.redditmedia.com/t5_122hf1/styles/communityIcon_h04bgma6k1p81.png"
+				title="Elden Ring"
+				last_played="3/6/23"
+				last_saved="3/6/23"
+				/>
+
+			<Card class="ma-3"
+				bg="https://thumbnails.pcgamingwiki.com/c/c0/Hollow_Knight_cover.jpg/300px-Hollow_Knight_cover.jpg" 
+				icon="https://styles.redditmedia.com/t5_122hf1/styles/communityIcon_h04bgma6k1p81.png"
+				title="Hollow Knight"
+				last_played="3/6/23"
+				last_saved="3/6/23"
+				/>
+
+			<Card class="ma-3"
+				bg="https://thumbnails.pcgamingwiki.com/3/39/Cyberpunk_2077_cover.png/300px-Cyberpunk_2077_cover.png" 
+				icon="https://styles.redditmedia.com/t5_122hf1/styles/communityIcon_h04bgma6k1p81.png"
+				title="Cyberpunk 2077"
+				last_played="3/6/23"
+				last_saved="3/6/23"
+				/>
+
+			<Card class="ma-3"
+				bg="https://thumbnails.pcgamingwiki.com/f/f1/Hitman_III_cover.jpg/300px-Hitman_III_cover.jpg" 
+				icon="https://styles.redditmedia.com/t5_122hf1/styles/communityIcon_h04bgma6k1p81.png"
+				title="Hitman III"
+				last_played="3/6/23"
+				last_saved="3/6/23"
+				/>
+
+			<Card class="ma-3"
+				bg="https://thumbnails.pcgamingwiki.com/7/72/Nier_Automata_cover.jpg/300px-Nier_Automata_cover.jpg" 
+				icon="https://styles.redditmedia.com/t5_122hf1/styles/communityIcon_h04bgma6k1p81.png"
+				title="NieR: Automata"
+				last_played="3/6/23"
+				last_saved="3/6/23"
+				/>
 			
 		</v-row>
 	</v-container>
