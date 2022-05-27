9456492a47dd231bd6a348a373320e41f827dca9
Mayank Tomar <40265635+0Mayank@users.noreply.github.com>
2022-05-27 17:17:26 +0530

    migrated to gsap from veloacity-animate

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,25 +1,27 @@
 <template>
     <v-hover v-slot="{ isHovering, props }">    
-        <div>    
-            <v-card width="310" class="rounded-card pa-1 ma-3" color="primary" v-bind="props">
-                <v-img class="rounded-card" cover aspect-ratio="0.6666667" :src="bg"></v-img>
-                
-                <transition
-                    @before-enter="beforeEnter"
-                    @enter="enter"
-                    @leave="leave"
-                    :css="false"
-                >
-                    <div v-if="isHovering" class="rounded-card overlay">   
-                        <!-- <div class="overlay rounded-card"></div> -->
+        
+        <v-card width="310" class="rounded-card pa-1 ma-3" color="primary" v-bind="props">
+            <v-img class="rounded-card" cover aspect-ratio="0.6666667" :src="bg"></v-img>
+            
+            <transition
+                @before-enter="beforeEnter"
+                @enter="enter"
+                @leave="leave"
+                :css="false"
+            >
+                <div v-if="isHovering" class="rounded-card overlay">   
+                    
+                    
+
+                    <div class="content">
                         <!-- <v-sheet class="game-icon rounded-card" width="60" color="primary" elevation="7">
                             <v-img class="rounded-card" width="60" :src="icon"></v-img>
                         </v-sheet> -->
-                        
 
-                        <v-card-title class="text-h5 font-weight-bold title">{{ title }}</v-card-title>
+                        <v-card-title class="text-h5 font-weight-bold">{{ title }}</v-card-title>
                         
-                        <v-card-actions class="actions" v-if="last_played">
+                        <v-card-actions v-if="last_played">
                             <v-btn class="act-btn" rounded>
                                 <v-icon>mdi-circle-medium</v-icon>
                                 Last Played: {{ last_played }}
@@ -31,15 +33,16 @@
                             </v-btn>
                         </v-card-actions>
                     </div>
-                </transition>
-                
-            </v-card>
-        </div>
+                </div>
+            </transition>
+            
+        </v-card>
+
     </v-hover>
 </template>
 
 <script lang="ts">
-import Velocity from 'velocity-animate'
+import gsap from 'gsap'
 
 export default {
     props: {
@@ -70,18 +73,24 @@ export default {
             el.style.height = '0em'
         },
         enter(el: any, done: any) {
-            Velocity(
-                el, 
-                {opacity: 1, height: '100%'},
-                {duration: 500, easing: 'easeOutCubic', complete: done}
-            )
+            gsap.to(
+                el, {
+                duration: 0.45,
+                opacity: 1, 
+                height: '100%',
+                ease: 'power1', 
+                onComplete: done
+            })
         },
         leave(el: any, done: any) {
-            Velocity(
-                el, 
-                {opacity: 0, height: '20%'},
-                {duration: 100, easing: 'easeInCubic', complete: done}
-            )
+            gsap.to(
+                el, {
+                duration: 0.19,
+                opacity: 0, 
+                height: '0em',
+                ease: 'power1', 
+                onComplete: done
+            })
         }
     }
 }
@@ -102,20 +111,12 @@ export default {
 
 .overlay{
     position: absolute;
-    top: 0;
+    bottom: 0;
     left: 0;
     right: 0;
-    bottom: 0;
-    width: 310;
     background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.7));
 }
-
-.title {
-    position: absolute;
-    bottom: 50px;
-}
-
-.actions {
+.content {
     position: absolute;
     bottom: 0px;
 }
